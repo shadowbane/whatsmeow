e63d02e64c54f492a0918bf059b5b86767740d7a
Adli I. Ifkar <adly.shadowbane@gmail.com>
2023-07-10 22:21:05 +0700

    Cleanup middleware

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -15,10 +15,6 @@ type List struct {
 	Middlewares map[string][]Middleware
 }
 
-//var MiddlewareList map[string][]middleware.Middleware
-
-//type Middleware = alice.Constructor
-
 // Chain - chains all middleware functions right to left
 // https://husobee.github.io/golang/http/middleware/2015/12/22/simple-middleware.html
 func Chain(f httprouter.Handle, m []Middleware) httprouter.Handle {
