54bcaae150db4603986b9f88367fde6a8a665291
Adli I. Ifkar <adly.shadowbane@gmail.com>
2023-07-10 22:21:38 +0700

    Added User model

---
+++ cmd/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type User struct {
+	ID          int64     `json:"id" gorm:"auto_increment;primary_key"`
+	Name        string    `json:"name" gorm:"Column:name;type:varchar(255);not null"`
+	Token       string    `json:"token" gorm:"Column:token;type:text;not null"`
+	Webhook     string    `json:"webhook" gorm:"Column:webhook;type:text"`
+	JID         string    `json:"jid" gorm:"Column:jid;type:varchar(255)"`
+	QRCode      string    `json:"qrcode" gorm:"Column:qrcode;type:text"`
+	IsConnected bool      `json:"is-connected" gorm:"Column:is_connected;type:tinyint(1);not null;default:0"`
+	Expiration  int64     `json:"expiration" gorm:"Column:expiration;type:integer"`
+	Events      string    `json:"events" gorm:"Column:events;type:varchar(255);not null;default:'All'"`
+	CreatedAt   time.Time `json:"created_at" gorm:"type:timestamp"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"type:timestamp"`
+}
+
+func (m *User) TableName() string {
+	return "users"
+}
