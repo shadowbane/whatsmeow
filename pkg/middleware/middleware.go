b7917d04b3bb507abe1b423ff2a9583ba12515e9
Adli I. Ifkar <adly.shadowbane@gmail.com>
2023-07-13 10:06:51 +0700

    Middlewares - Refactoring, sorted methods

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -12,43 +12,12 @@ type Middleware struct {
 	Handler func(httprouter.Handle) httprouter.Handle
 }
 
-type List struct {
-	Middlewares map[string][]Middleware
-}
-
 type MwStruct struct {
 	Models *gorm.DB
 }
 
-// Chain - chains all middleware functions right to left
-// https://husobee.github.io/golang/http/middleware/2015/12/22/simple-middleware.html
-func Chain(f httprouter.Handle, m []Middleware) httprouter.Handle {
-	// if our chain is done, use the original handlerfunc
-	if len(m) == 0 {
-		return f
-	}
-
-	// otherwise run recursively over nested handlers
-	return m[0].Handler(Chain(f, m[1:]))
-}
-
-func InitMiddlewareList(db *gorm.DB) *List {
-	list := &List{
-		Middlewares: make(map[string][]Middleware),
-	}
-
-	mw := &MwStruct{
-		Models: db,
-	}
-
-	list = list.Set("default", []Middleware{
-		{Handler: mw.LogRequest},
-	}).Set("auth", []Middleware{
-		{Handler: mw.Auth},
-		{Handler: mw.LogRequest},
-	})
-
-	return list
+type List struct {
+	Middlewares map[string][]Middleware
 }
 
 func (m *List) Set(name string, middlewares []Middleware) *List {
@@ -68,31 +37,6 @@ func (m *List) Chain(f httprouter.Handle, name ...string) httprouter.Handle {
 
 	// run original function
 	return Chain(f, middlewares)
-}
-
-// Unique removes duplicates from a slice of Middleware
-func Unique(middlewares []Middleware) []Middleware {
-	unique := make([]Middleware, 0, len(middlewares))
-
-	found := false
-
-	for _, v := range middlewares {
-		found = false
-
-		for _, u := range unique {
-			// compare u.Handler and v.Handler
-			if reflect.ValueOf(u.Handler).Pointer() == reflect.ValueOf(v.Handler).Pointer() {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			unique = append(unique, v)
-		}
-	}
-
-	return unique
 }
 
 func (m *List) Get(name string) []Middleware {
@@ -131,4 +75,60 @@ func (m *List) Prepend(to string, middlewares ...Middleware) *List {
 	tmp = nil
 
 	return m
+}
+
+// Chain - chains all middleware functions right to left
+// https://husobee.github.io/golang/http/middleware/2015/12/22/simple-middleware.html
+func Chain(f httprouter.Handle, m []Middleware) httprouter.Handle {
+	// if our chain is done, use the original handlerfunc
+	if len(m) == 0 {
+		return f
+	}
+
+	// otherwise run recursively over nested handlers
+	return m[0].Handler(Chain(f, m[1:]))
+}
+
+func InitMiddlewareList(db *gorm.DB) *List {
+	list := &List{
+		Middlewares: make(map[string][]Middleware),
+	}
+
+	mw := &MwStruct{
+		Models: db,
+	}
+
+	list = list.Set("default", []Middleware{
+		{Handler: mw.LogRequest},
+	}).Set("auth", []Middleware{
+		{Handler: mw.Auth},
+		{Handler: mw.LogRequest},
+	})
+
+	return list
+}
+
+// Unique removes duplicates from a slice of Middleware
+func Unique(middlewares []Middleware) []Middleware {
+	unique := make([]Middleware, 0, len(middlewares))
+
+	found := false
+
+	for _, v := range middlewares {
+		found = false
+
+		for _, u := range unique {
+			// compare u.Handler and v.Handler
+			if reflect.ValueOf(u.Handler).Pointer() == reflect.ValueOf(v.Handler).Pointer() {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			unique = append(unique, v)
+		}
+	}
+
+	return unique
 }
